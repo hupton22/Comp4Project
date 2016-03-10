f8dc4581420d195303a3e14593c180a0a7d56188
hupton22 <hupton22@btinternet.com>
2016-03-10 00:39:29 +0000

    nearly finished comments

--- Comp4 Project/Comp4 Project/Form1.cs
+++ Comp4 Project/Comp4 Project/Form1.cs
@@ -18,8 +18,8 @@ namespace Comp4_Project
 
         //public int atomNumber = 20;
         
-        Atom[] atoms = new Atom[84]; 
-        List<Neutron> neutronList = new List<Neutron>(); 
+        Atom[] atoms = new Atom[84]; //initialise the array of atoms
+        List<Neutron> neutronList = new List<Neutron>(); //initialise the list of 
 
         public double realDist = 0;
 
@@ -28,19 +28,19 @@ namespace Comp4_Project
             InitializeComponent();
         }
 
-        private double DegreeToRadian(int angle)
-        {
-            return Math.PI * angle / 180.0;
+        private double DegreeToRadian(int angle)//this function is not actually used. It was part of an attempt to give all of the neutrons the same velocity...
+        {//whicle still sending them off at random angles. this wouild usee trigonometry, and so a function was needed to convert angles measured in degrees to angles in radians...
+            return Math.PI * angle / 180.0;//which are the default angle unit in c#
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Random random = new Random();
+            Random random = new Random();//create a new random object
 
             int startNeutrons = 1;
             for (int i = 0; i < startNeutrons; i++)
             {
-                Neutron neutron = new Neutron();
+                Neutron neutron = new Neutron();//create a new neutron object
 
                 //int angle = (random.Next(1, 360));
 
@@ -48,35 +48,35 @@ namespace Comp4_Project
                 //double a = ();
                 //neutron.SetVelocityX(Math.Cos(DegreeToRadian(angle)));
 
-                neutron.SetVelocityX(random.Next(1, 5));
-                neutron.SetVelocityY(random.Next(1, 5));
+                neutron.SetVelocityX(random.Next(1, 5));//select a random velocity in the x direction
+                neutron.SetVelocityY(random.Next(1, 5));//same in the y
 
                 neutron.SetXPos(random.Next(0, ClientSize.Width - Neutron.NeutronWidth));
                 neutron.SetXPos(random.Next(0, ClientSize.Height - Neutron.NeutronWidth));
 
-                neutronList.Add(neutron);
+                neutronList.Add(neutron);//adding the new neutron to the list
             }
                         
-            int numberOfAtomsPerLine = 12;
-            int numberOfAtomsPerRow = 7;
-            int xDiff = (ClientSize.Width - 50) / numberOfAtomsPerLine; //to calculate the difference in the width of each x of the atoms
-            int yDiff = (ClientSize.Height - 50) / numberOfAtomsPerRow; //to calculate the difference in the height of each x of the atom
+            int numberOfAtomsPerRow = 12;//difining the size of the grid
+            int numberOfAtomsPerColumn = 7;
+            int xDiff = (ClientSize.Width - 50) / numberOfAtomsPerRow; //to calculate the difference in the width of each x of the atoms
+            int yDiff = (ClientSize.Height - 50) / numberOfAtomsPerColumn; //to calculate the difference in the height of each x of the atom
 
             int x = xDiff; //The x-coordinate of the paint area
             int y = yDiff; //The y-coordinate of the paint area
             int counter = 0;
 
-            for (int i = 0; i < atoms.Length; i++)
+            for (int i = 0; i < atoms.Length; i++)//loop for placing the atoms in a grid
             {
                 //start a new row
-                if (counter >= numberOfAtomsPerLine)
+                if (counter >= numberOfAtomsPerRow)
                 {
                     y = y + yDiff;
                     x = xDiff;
                     counter = 0; //reset the counter
                 }
 
-                atoms[i] = new Atom(x, y);
+                atoms[i] = new Atom(x, y);//populate the array with atom objects
                 x = x + xDiff;
                 
                 counter++;
@@ -91,7 +91,7 @@ namespace Comp4_Project
         
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        private void Form1_Paint(object sender, PaintEventArgs e)//this procedure handles updating of the screen
         {
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             e.Graphics.Clear(BackColor);
@@ -99,13 +99,23 @@ namespace Comp4_Project
             drawBalls(e);
         }
 
-        private void timerMoveBall_Tick(object sender, EventArgs e)
+        private void timerMoveBall_Tick(object sender, EventArgs e)//this procedure is executed every time the timer "ticks"
         {
-            moveBalls();//look down
+            moveBalls();
             Refresh();
         }
 
-        private void moveBalls()
+        //private int generateSign()
+        //{
+        //    Random random = new Random();
+        //    int sign = random.Next(-1, 1); 
+        //    if(sign == 0)
+        //    {
+        //        generateSign
+        //    }
+        //}
+
+        private void moveBalls()//procedure to move all neutrons
         {
             
             foreach (Neutron neutron in neutronList)
@@ -113,16 +123,16 @@ namespace Comp4_Project
                 neutron.move(ClientSize.Width, ClientSize.Height); //Move all neutrons first
             }
 
-            List<Neutron> newNeutrons = new List<Neutron>();
+            List<Neutron> newNeutrons = new List<Neutron>();//temporary neutron list
             Random random = new Random();
             foreach (Particle atom in atoms)
             {
                 foreach (Particle neutron in neutronList)
                 {
-                    if (atom.InteractsWith(neutron))
+                    if (atom.InteractsWith(neutron))//only trigger this block if the neutron interacts with an atom
                     {
-                        //neturons can be created in here
-                        int xPos = atom.GetXPos();
+                        
+                        int xPos = atom.GetXPos();//retrieve the coordinates of the atom that is splitting
                         int yPos = atom.GetYPos();
 
                         for (int i = 0; i < 2; i++)
@@ -130,20 +140,20 @@ namespace Comp4_Project
                             int vX = random.Next(1, 5);
                             int vY = random.Next(1, 5);
 
-                            Neutron newNeutron = new Neutron();
-                            newNeutron.SetXPos(xPos);
+                            Neutron newNeutron = new Neutron();//create a  new neutron object
+                            newNeutron.SetXPos(xPos);//places the new neutron in the location of the atom that is splitting
                             newNeutron.SetYPos(yPos);
                             newNeutron.SetVelocityX(vX);
                             newNeutron.SetVelocityY(vY);
 
-                            newNeutrons.Add(newNeutron);
+                            newNeutrons.Add(newNeutron);//letting the new neutron become a menber of the temp neutron list
                         }
                     }
                 }
             }
 
       
-            neutronList.AddRange(newNeutrons);
+            neutronList.AddRange(newNeutrons);//adding the contents of the temp list to the main list
         }
 
  
@@ -156,7 +166,7 @@ namespace Comp4_Project
                 e.Graphics.DrawEllipse(Pens.Black, ball.GetXPos(), ball.GetYPos(), Neutron.NeutronWidth, Neutron.NeutronWidth);
             }
 
-            foreach (Atom a in atoms)
+            foreach (Atom a in atoms)//draw all atoms as circles with the same diamiter as the width of the atom
             {
                 e.Graphics.FillEllipse(a.PickBrush(), a.GetXPos(), a.GetYPos(), Atom.AtomWidth, Atom.AtomWidth);
                 e.Graphics.DrawEllipse(Pens.Black, a.GetXPos(), a.GetYPos(), Atom.AtomWidth, Atom.AtomWidth);
--- Comp4 Project/Comp4 Project/Particles/Particle.cs
+++ Comp4 Project/Comp4 Project/Particles/Particle.cs
@@ -15,16 +15,14 @@ namespace Comp4_Project.Particles
         {
         }
 
-        //Just subclasses should see this
+        
         protected Particle(int xPos, int yPos)
         {
             this.xPos = xPos;
             this.yPos = yPos;
         }
 
-        /**
-         *  Getter and setter for position
-         */
+        //Getters and setters for x and y coordinates
         public void SetXPos(int xPos)
         {
             this.xPos = xPos;
@@ -51,18 +49,18 @@ namespace Comp4_Project.Particles
 
         protected abstract Boolean IsDisabled();
 
-        public virtual Boolean InteractsWith(Particle otherParticle)
-        {
-            double minX = this.GetXPos();
+        public virtual Boolean InteractsWith(Particle otherParticle)//this function returns a boolean "true" if the neutron in question is within a cetrain distance of any atoms and ...
+        {//... if the atom that it was found to be close to has not split already
+            double minX = this.GetXPos();//retrieve the coordinates of the particle in quastion
             double maxX = minX + this.GetSize();
 
             double minY = this.GetYPos();
             double maxY = minY + this.GetSize();
 
-            Boolean inXRange = (minX <= otherParticle.GetXPos()) && (otherParticle.GetXPos() <= maxX);
+            Boolean inXRange = (minX <= otherParticle.GetXPos()) && (otherParticle.GetXPos() <= maxX);//detects if the neutron is within a square area surroiunding the location of the atom
             Boolean inYRange = (minY <= otherParticle.GetYPos()) && (otherParticle.GetYPos() <= maxY);
 
-            Boolean result = inXRange && inYRange;
+            Boolean result = inXRange && inYRange;//the result is the logical and of whether the particle is within the y range and the x range
             Boolean disabled = this.IsDisabled();
 
             if (result)
@@ -73,7 +71,7 @@ namespace Comp4_Project.Particles
             return result && !disabled;
         }
 
-        public double DistanceTo(Particle otherParticle)
+        public double DistanceTo(Particle otherParticle)//this calculates and returns the distance between two particle objects usin pythagoras's theorum a^2 + b^2 = c^2
         {   
             double xDist = this.GetXPos() - otherParticle.GetXPos();
             double yDist = this.GetYPos() - otherParticle.GetYPos();
